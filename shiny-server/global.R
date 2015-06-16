06d2be6a53821956ca85b62c0e05b3c5eb5dc5ef
Julian Winternheimer <julianwinternheimer@gmail.com>
2015-06-16 10:50:40 -0500

    Added code from plotlyGraphWidget.R
    Needed to render plotly graphs. :)

--- shiny-server/global.R
+++ shiny-server/global.R
@@ -1,6 +1,7 @@
 library(data.table)
 library(dplyr)
 library(tidyr)
+library(plotly)
 
 
 cleanGoogleTable <- function(dat, table=1, skip=0, ncols=NA, nrows=-1, header=TRUE, dropFirstCol=NA){
@@ -50,7 +51,7 @@ urls <- function() {
 
 readGoogleSheet <- function(url, name, na.string="", header=TRUE){
     day <-format(Sys.time(), "%Y-%m-%d")
-    filename <- paste0('/data/','foo',name,day,'.csv')
+    filename <- paste0('./data/',name,day,'.csv')
      if (!file.exists(filename))
         download(url, destfile=filename)
 
@@ -108,4 +109,38 @@ readData <- function (key='team') {
 
     d <- readGoogleSheet(u[key], key)
     cleanUpData(d)
+}
+
+#This script is necessary in the app folder
+#but the user should not have to edit it
+
+#Output Graph Function
+graphOutput <- function(inputId, width="100%", height="550px") {
+  tagList(
+    singleton(tags$head(
+      tags$script(src="plotlyGraphWidget.js")
+    )),
+    tags$iframe(id=inputId, src="https://plot.ly/~playground/7.embed",
+                class="graphs", style="border:none;", seamless=TRUE, width=width, height=height)
+  )
+}
+
+renderGraph <- function(expr, env=parent.frame(), quoted=FALSE) {
+  ## This gets called when inputs change --
+  ## Place data wrangling code in here
+  ## and pass the result to the client
+  ## to be graphed.
+  
+  installExprFunction(expr, "func", env, quoted)
+  
+  function(){
+    data = func();
+    ## data is the state of the widgets: see server.R
+    ## this function returns a list of named lists that descripe
+    ## valid postMessage commands to be sent to the embedded iframe.
+    ## see binding.renderValue for the receiving JS side of this function
+    ## and https://github.com/plotly/Embed-API for more about the postMessage
+    ## graph messages
+    return(data)
+  }
 }
