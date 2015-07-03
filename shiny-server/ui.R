31b7da90dcd1b31c9d7b95db4728d55302d5c5ba
Michael Erasmus <michaelerasmus@gmail.com>
2015-07-03 13:00:32 +0200

    Add social sharing button, fix CTA css and remove debug tab

--- shiny-server/ui.R
+++ shiny-server/ui.R
@@ -16,8 +16,12 @@ shinyUI(
                                     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
                                     ga('create', 'UA-18896347-6', 'auto');
                                     ga('send', 'pageview');
-                                    ")
-                        ),
+                                    "),
+                            tags$meta(property="og:title", content="Buffer Open Source Diversity Dashboard"),
+                            tags$meta(property="og:url", content="http://diversity.buffer.com"),
+                            tags$meta(property="og:description", content="What gets measured, gets managed. With this dashboard, we’re sharing real-time data on the demographic diversity of the Buffer team, as well as those who’ve expressed interest in joining our team, in the specific areas of age, gender, and ethnicity."),
+                            tags$meta(property="og:image", content="https://open.bufferapp.com/wp-content/uploads/2015/06/pablo-23.png")
+                      ),
                       tags$link(rel = "stylesheet", type = "text/css", href = "button.css"),
                       sidebarLayout(
                         sidebarPanel(
@@ -99,12 +103,34 @@ shinyUI(
                           ),
                           tags$div(style="margin: auto",
                                    tags$h3(style="text-align:center;margin-top: 40px;","Buffer is the easiest way to save time on social media"),
-                                   tags$button(class="buffer-button large buffer-btn-primary hero-cta buffer-btn-rounded",
+                                   tags$button(class="buffer-button-cta large buffer-btn-primary hero-cta buffer-btn-rounded",
                                                tags$a(href="https://bufferapp.com/?utm_source=traction&utm_medium=DiversityDashboard&utm_campaign=sidebar-sign-up",
                                                       "Start Scheduling Posts on Social Media")
                                    )
-                          )
-                        ),
+                          ),
+                          tags$div(id="fb-root"),
+                          tags$script("(function(d, s, id) {
+                              var js, fjs = d.getElementsByTagName(s)[0];
+                                if (d.getElementById(id)) return;
+                                js = d.createElement(s); js.id = id;
+                                  js.src = '//connect.facebook.net/en_US/all.js#xfbml=1&appId=103667826405103';
+                                  fjs.parentNode.insertBefore(js, fjs);
+                              }(document, 'script', 'facebook-jssdk'))"),
+                          tags$script(src="https://apis.google.com/js/plusone.js"),
+                          tags$div(HTML('<div class="share">
+                                           <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?"http":"https";if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document, "script", "twitter-wjs");</script>
+                                          <div class="grid-row">
+                                              <div class="">
+                                                  <div class="social-sharing-buttons-centered">
+                                                      <div class="fb-like" data-send="false" data-layout="button_count" data-width="90" data-show-faces="false" style="width:83px; overflow:hidden;position:relative; top:auto;  bottom:0px; left:3px;"></div>
+                                                      <a href="https://twitter.com/share" class="twitter-share-button" data-via="buffer" data-text="We\'re building a more inclusive Buffer! Take a look at our diversity dashboard">Tweet</a>
+                                                      <g:plusone size="medium"></g:plusone>
+                                                      <a href="http://bufferapp.com/add" class="buffer-add-button" data-text="We&#39;re building a more inclusive Buffer! Take a look at our diversity dashboard" data-url="http:&#x2F;&#x2F;diversity.buffer.com" data-count="horizontal" data-via="buffer" ></a><script type="text/javascript" src="https://d389zggrogs7qo.cloudfront.net/js/button.js"></script>
+                                                  </div>
+                                              </div>
+                                          </div>
+                                         </div>'))
+                              ),
                         mainPanel(
                           fluidRow(
                             column(width=10,
@@ -120,7 +146,7 @@ shinyUI(
                                    )
                             )
                           ),
-                          
+
                           #ethnicity
                           fluidRow(
                             column(width=10,
@@ -151,7 +177,7 @@ shinyUI(
                                    )
                             )
                           )
-                          
+
                         )
                       )
                       ),
@@ -163,11 +189,11 @@ shinyUI(
                         tableOutput('applicantsTable')
                       )
              )
-             ,tabPanel('Debug',
-                       mainPanel(
-                         tableOutput('debugTable1'),
-                         tableOutput('debugTable2')
-                       )
-             )
+             #,tabPanel('Debug',
+             #          mainPanel(
+             #            tableOutput('debugTable1'),
+             #            tableOutput('debugTable2')
+             #          )
+             #)
              )
 )
