460290727ba6b31c7c6145d7107671ccb31a3b7a
Oscar Sanchez Casamitjana <oscar@ustwo.com>
2016-08-16 13:22:33 +0100

    Update GA to use ustwo

--- shiny-server/ui.R
+++ shiny-server/ui.R
@@ -14,8 +14,7 @@ shinyUI(
                                     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
                                     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
                                     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-                                    ga('create', 'UA-18896347-6', 'auto');
-                                    ga('send', 'pageview');
+                                    ga('create', 'UA-7940378-1', 'auto');
                                     "),
                             tags$meta(property="og:title", content="ustwo Diversity Dashboard"),
                             tags$meta(property="og:url", content="http://ustwo.buffer.com"),
